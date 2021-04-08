c1cc0c5a255799707d2bb2b94977485b799e1ca0
Rohit <rohitgta7@gmail.com>
2021-04-08 22:03:14 +0530

    Today I have added styling in my vue assignment

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template lang="html">
   <div id="app">
-    <h1>NewsFeed</h1>
-    <button @click="togglePanel()">Add Post</button>
+    <h1>
+      NewsFeed
+      <button class="add-post" @click="togglePanel()">Add Post</button>
+    </h1>
     <hr />
-    <div v-for="(post) in posts" :key="post.id">
-      <h1>{{post.userName}}</h1>
-      <h2>{{post.title}}</h2>
-      <h3>{{post.body}}</h3>
-      <button @click="getPostData(post)">Edit</button> |
-      <button @click="toggleDeletePanel(post)">Delete</button> |
-      <button @click="toggleViewPanel(post)">View</button>
-      <hr />
+    <div class="list" v-for="(post) in posts" :key="post.id">
+      <h3>
+        {{post.userName}}
+        <button class="button" :style="{background: '#dc3545'}" @click="toggleDeletePanel(post)">Delete</button>
+        <button class="button" :style="{background: 'rgb(9 168 199)'}" @click="getPostData(post)">Edit</button>
+        <button class="button" :style="{background: '#198754'}" @click="toggleViewPanel(post)">View</button>
+      </h3>
+      <h4>{{post.title}}</h4>
+      <p>{{post.body}}</p>
     </div>
     <div class="modal" v-if="showAddPost">
       <div class="modal-content">
@@ -18,31 +21,34 @@
         <h3>Add New Post</h3>
         <hr />
         <form>
-          <label>UserName</label><br>
+          <label class="label">UserName:</label><br>
           <input
             type="text"
             id="userName"
             v-model="postData.userName"
             autoComplete="off"
+            class="input-box"
             >
           <br>
-          <label>Title</label><br>
+          <label class="label">Title:</label><br>
           <input
             type="text"
             v-model="postData.title"
             id="title"
             autoComplete="off"
+            class="input-box"
           >
           <br>
-          <label>Description</label><br>
+          <label class="label">Description:</label><br>
           <input
             type="text"
             v-model="postData.body"
             id="body"
             autoComplete="off"
+            class="description-box"
           >
           <br>
-          <button @click.prevent="createPost()" type="submit">Submit</button>
+          <button class="submit-btn" @click.prevent="createPost()" type="submit">Add Post</button>
         </form>
       </div>
     </div>
@@ -52,28 +58,31 @@
         <h3>Edit the Post</h3>
         <hr />
         <form>
-          <label>UserName</label><br>
+          <label class="label" >UserName</label><br>
           <input
             type="text"
             v-model="postData.userName"
             id="userName"
+            class="input-box"
             >
           <br>
-          <label>Title</label><br>
+          <label class="label" >Title</label><br>
           <input
             type="text"
             v-model="postData.title"
             id="title"
+            class="input-box"
           >
           <br>
-          <label>Description</label><br>
+          <label class="label" >Description</label><br>
           <input
             type="text"
             v-model="postData.body"
             id="body"
+            class="input-box"
           >
           <br>
-          <button @click.prevent="updatePost(postData)" type="submit">Update</button>
+          <button class="update-btn" @click.prevent="updatePost(postData)" type="submit">Update</button>
         </form>
       </div>
     </div>
@@ -82,9 +91,15 @@
         <span @click="showDeletePost = false" class="close">&times;</span>
         <h3>Delete the post</h3>
         <hr />
-        <h1>Are you sure you want to delete the post?</h1>
-        <button @click.prevent="deletePost(postData)" type="submit">Yes</button> |
-        <button @click="showDeletePost = false" type="submit">No</button>
+        <p :style="{margin: '20px 0 10px 0'}">Are you sure you want to delete the post?</p>
+        <button class="submit-btn" :style="{background: '#dc3545'}"
+         @click.prevent="deletePost(postData)" type="submit">
+         Yes
+        </button>
+        <button class="submit-btn"
+         @click="showDeletePost = false" type="submit">
+         No
+        </button> 
       </div>
     </div>
     <div class="modal" v-if="showViewPost">
@@ -92,30 +107,40 @@
         <span @click="viewClosePanel()" class="close">&times;</span>
         <h3>View Post</h3>
         <hr />
-        <h1>{{this.postData.userName}}</h1>
-        <h2>{{this.postData.title}}</h2>
-        <h3>{{this.postData.body}}</h3>
-        <div v-for="comment in comments" :key="comment.id">
-          <h2>
-            {{comment.author}}<br>
-            {{comment.message}}
-          </h2>
+        <div class="list">
+          <h3>{{this.postData.userName}}</h3>
+          <h4>{{this.postData.title}}</h4>
+          <p>{{this.postData.body}}</p>
         </div>
-        <label>Author</label><br>
+        <div 
+          :style="{padding: '5px 0 5px 7px', display: 'flex', background: 'white', margin: '5px'}" 
+          v-for="comment in comments" :key="comment.id">
+          <img 
+            :src="`https://joeschmoe.io/api/v1/${comment.author}`"
+            :style="{margin: '8px 5px 0 0', background: 'lightblue', borderRadius: '0.4rem'}" 
+          width="40px" height="40px" alt={comment.author}/>
+          <div>
+            <h3>{{comment.author}}</h3>
+            <p>{{comment.message}}</p>
+          </div>
+        </div>
+        <label class="label" >Author</label><br>
         <input
           type="text"
           id="name"
           v-model="commentData.author"
           autoComplete="off"
+          class="input-box"
         ><br>
-        <label>Comment</label><br>
+        <label class="label" >Comment</label><br>
         <input
           type="text"
           id="comment"
           v-model="commentData.message"
           autoComplete="off"
+          class="input-box"
         ><br>
-        <button @click.prevent="submitComment()" type="submit">Comment</button>
+        <button class="submit-btn" @click.prevent="submitComment()" type="submit">Comment</button>
       </div>
     </div>
   </div>
@@ -171,13 +196,13 @@ export default {
       this.postData.id = post.id
     },
     toggleViewPanel (post) {
-      this.showViewPost = true
-      this.postData.id = post.id
       this.commentData.cId = post.id
-      this.fetchComments()
+      this.postData.id = post.id
       this.postData.userName = post.userName
       this.postData.title = post.title
       this.postData.body = post.body
+      this.fetchComments()
+      this.showViewPost = true
     },
     editClosePanel (postData) {
       axios.put(`http://localhost:3001/posts/${postData.id}`, {
@@ -267,8 +292,138 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  margin: auto 5%;
+  padding: 1%;
+  border-radius: 1%;
+  background-color: #f6f6f6;
+}
+
+.input-box {
+    width: 95%;
+    padding: 0.3rem 0.6rem;
+    font-size: 1rem;
+    line-height: 1.5;
+    color: #495057;
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: .3rem;
+    margin-bottom: 5px;
+}
+
+.label {
+  font-size: inherit;
+  line-height: 1.5;
+}
+
+.description-box {
+  width: 95%;
+  padding: 0.3rem 0.6rem;
+  font-size: 1rem;
+  line-height: 1.5;
+  color: #495057;
+  background-color: #fff;
+  background-clip: padding-box;
+  border: 1px solid #ced4da;
+  border-radius: .3rem;
+}
+
+.button {
+  color: white;
+  font-size: 0.9rem;
+  float: right;
+  cursor: pointer;
+  border: none;
+  background: blue;
+  padding: 10px 15px;
+  font-weight: 700;
+  line-height: 1em;
+  margin-right: 5px;
+  border-radius: 0.4rem;
+}
+
+.submit-btn {
+  color: white;
+  font-size: 0.9rem;
+  cursor: pointer;
+  border: none;
+  background: blue;
+  padding: 10px 15px;
+  font-weight: 700;
+  line-height: 1.3em;
+  margin: 10px 7px 0 0;
+  border-radius: 0.3rem;
+}
+
+.update-btn{
+  color: white;
+  font-size: 0.9rem;
+  cursor: pointer;
+  border: none;
+  background: rgb(212, 139, 3);
+  padding: 10px 15px;
+  font-weight: 700;
+  line-height: 1.3em;
+  margin: 10px 0 0 0;
+  border-radius: 0.3rem;
+}
+
+.update-btn:hover {
+  -webkit-transform: scale(1.1);
+  -moz-transform: scale(1.1);
+  -o-transform: scale(1.1);
+  background: rgb(226, 149, 5);
+} 
+
+.submit-btn:hover {
+  -webkit-transform: scale(1.1);
+  -moz-transform: scale(1.1);
+  -o-transform: scale(1.1);
+  background: #006BF5;
+}   
+
+.button:hover {
+  -webkit-transform: scale(1.1);
+  -moz-transform: scale(1.1);
+  -o-transform: scale(1.1);
+  background: #006BF5;
+}   
+
+.add-post {
+  color: white;
+  font-size: 1rem;
+  float: right;
+  cursor: pointer;
+  border: none;
+  background: blue;
+  padding: 13px 20px;
+  font-weight: 800;
+  line-height: 1em;
+  border-radius: 0.4rem;
+}
+
+.add-post:hover {
+  -webkit-transform: scale(1.1);
+  -moz-transform: scale(1.1);
+  -o-transform: scale(1.1);
+  background: #0078F5;
+}   
+
+.list {
+  margin: 5px;
+  background: white;
+  padding: 10px;
+  border-radius: 15px;
+  pointer-events: auto;
+}
+
+.list:hover {
+  background: #f9f9f9
+}
+
+h3, h4, p {
+  margin: 5px 0 5px 0;
 }
 
 .borderStyle {
@@ -280,11 +435,14 @@ export default {
 }
 
 .modal-content {
-  background-color: #fefefe;
-  margin: 1% auto;
+  background-color: #f6f6f6;
+  margin: 1% 20%;
   padding: 20px;
   border: 1px solid #888;
-  width: 60%;
+  pointer-events: auto;
+  background-clip: padding-box;
+  border: 1px solid rgba(0,0,0,.2);
+  border-radius: .3rem;
 }
 
 .close {
@@ -296,11 +454,13 @@ export default {
 
 .modal {
   position: fixed;
-  top: 2px;
+  top: 0;
   bottom: 0;
   left: 0;
   right: 0;
   background: rgba(0,0,0,0.6);
+      overflow-x: hidden;
+    overflow-y: auto;
 }
 
 .close:hover,
